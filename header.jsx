a653ebb7832d669cf4b7893320e14d5057557321
NITIN GUPTA <SUPERFULL03213@GMAIL.COM>
2026-02-20 16:46:08 +0530

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <input type="text" placeholder="Search..." />
+      <div className="profile">Admin User</div>
+    </header>
+  );
+};
+
+export default Header;
